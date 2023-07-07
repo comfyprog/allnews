101d0c63b7a44c85993658d06e820fd3b1a66c00
Alex Nesterenko <comfyprog@proton.me>
2023-07-07 17:33:08 +0300

    extract articles from feed

--- feed.go
+++ feed.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"time"
 
 	"github.com/mmcdole/gofeed"
 )
+
+type article struct {
+	resource    string
+	url         string
+	title       string
+	published   *time.Time
+	description string
+	item        []byte
+}
 
 func getFeed(url string, timeout time.Duration) (*gofeed.Feed, error) {
 	parser := gofeed.NewParser()
@@ -13,4 +23,25 @@ func getFeed(url string, timeout time.Duration) (*gofeed.Feed, error) {
 	defer cancel()
 
 	return parser.ParseURLWithContext(url, ctx)
+}
+
+func extractArticles(feed *gofeed.Feed, resource string) ([]article, error) {
+	articles := make([]article, 0, len(feed.Items))
+
+	for _, item := range feed.Items {
+		itemData, err := json.Marshal(item)
+		if err != nil {
+			return articles, err
+		}
+		articles = append(articles, article{
+			resource:    resource,
+			url:         item.Link,
+			title:       item.Title,
+			description: item.Description,
+			published:   item.PublishedParsed,
+			item:        itemData,
+		})
+	}
+
+	return articles, nil
 }
--- feed_test.go
+++ feed_test.go
@@ -80,3 +80,32 @@ func TestGetFeedUnparseable(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Nil(t, feed)
 }
+
+func TestExtractArticles(t *testing.T) {
+	data, err := os.ReadFile("./testdata/rss1.xml")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("content-type", "text/xml;charset=UTF-8")
+		w.Write(data)
+	}))
+
+	feed, err := getFeed(srv.URL, time.Millisecond)
+	assert.Nil(t, err)
+	assert.NotNil(t, feed)
+
+	articles, err := extractArticles(feed, "site1")
+	assert.Nil(t, err)
+
+	assert.Len(t, articles, len(feed.Items))
+
+	for _, a := range articles {
+		t.Run(a.url, func(t *testing.T) {
+			assert.Equal(t, "site1", a.resource)
+			assert.Greater(t, len(a.item), 0)
+		})
+	}
+
+}
